76afcb4ae5df086d2fa145b2bcb1bb7732c3328d
gpdavila <gpiscoya2013@hotmail.com>
2018-01-17 14:38:15 +0000

    Energy Measurements

---
+++ build/CMakeFiles/jtx1inst.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jtx1inst.dir/src/jtx1temp.c.o"
+  "CMakeFiles/jtx1inst.dir/src/jtx1pow.c.o"
+  "CMakeFiles/jtx1inst.dir/src/jtx1rate.c.o"
+  "libjtx1inst.pdb"
+  "libjtx1inst.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jtx1inst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/jtx1inst_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jtx1inst_demo.dir/samples/jtx1inst_demo.c.o"
+  "jtx1inst_demo.pdb"
+  "jtx1inst_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jtx1inst_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
